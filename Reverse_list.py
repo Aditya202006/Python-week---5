051b4a318d4645c7e968bef3cec8fa4d8db6cb2b
Aditya202006 <adityakanchana@gmail.com>
2024-08-30 11:26:54 +0530

    Create Reverse_list.py

---
+++ Reverse_list.py
@@ -0,0 +1,11 @@
+// Write a program to reverse a list in python
+
+l=[]
+s=int(input('Enter no.of elements of list:'))
+for i in range(s):
+	ele=int(input('Enter elements:'))
+	l.append(ele)
+n=l[::-1]
+print(n)
+m=l.reverse()
+print(m)
