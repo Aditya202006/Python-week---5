61fa6fb460ad948a103406d310b7c9ce3094bfc5
Aditya202006 <adityakanchana@gmail.com>
2024-08-30 11:35:07 +0530

    Reverse_list.py

--- Reverse_list.py
+++ Reverse_list.py
@@ -9,3 +9,10 @@ n=l[::-1]
 print(n)
 m=l.reverse()
 print(m)
+
+''' output : Enter no.of elements of list:4
+	 Enter elements:45
+	 Enter elements:18
+	 Enter elements:3
+	 Enter elements:7
+	 [7,3,18,45] 
